8b4b6a85f330c03b9ebda177bf5cc86c7f4b101b
Marinko <marinkospasojevic@gmail.com>
2021-06-17 13:07:48 +0200

    Update to .NET 5

--- SwaggerUI/Startup.cs
+++ SwaggerUI/Startup.cs
@@ -56,14 +56,13 @@ namespace SwaggerUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseStaticFiles();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
